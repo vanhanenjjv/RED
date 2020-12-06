4280f36a5cf9ea5ee69c8c7ffe3c6d3b25e1cd02
Janne Vanhanen <janne.vanhanen@outlook.com>
2020-12-07 00:42:53 +0200

    Add pn532 library and working example

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,12 +1,41 @@
 #include <Arduino.h>
+#include <pn532.h>
+#include <pn532_uno.h>
+
+uint8_t buff[255];
+uint8_t uid[MIFARE_UID_MAX_LENGTH];
+int32_t uid_len = 0;
+PN532 pn532;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  PN532_SPI_Init(&pn532);
+  
+  if (PN532_GetFirmwareVersion(&pn532, buff) == PN532_STATUS_OK) {
+    Serial.print("Found PN532 with firmware version: ");
+    Serial.print(buff[1], DEC);
+    Serial.print(".");
+    Serial.println(buff[2], DEC);
+    Serial.println("Waiting for RFID/NFC card...");
+  }
+
+  PN532_SamConfiguration(&pn532);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("yeet");
-  delay(1000);
+  // Check if a card is available to read
+  uid_len = PN532_ReadPassiveTarget(&pn532, uid, PN532_MIFARE_ISO14443A, 1000);
+
+  if (uid_len == PN532_STATUS_ERROR) return;
+
+  Serial.print("Found card with UID: ");
+  for (uint8_t i = 0; i < uid_len; i++) {
+    if (uid[i] <= 0xF) {
+      Serial.print("0");
+    }
+    Serial.print(uid[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
 }
