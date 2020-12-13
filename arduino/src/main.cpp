5787cb731c1a1e764c2e838bc3767d87e6a762d1
Sakari Mäkinen <sakari.makinen@gmail.com>
2020-12-13 07:50:06 +0200

    add card reading functionality

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -17,27 +17,37 @@
  *Button to D2
 */
 
-// include the library code:
-#include <LiquidCrystal.h>
+// include the library code: 
+#include <LiquidCrystal.h> 
+#include <Arduino.h>
+#include <Adafruit_PN532.h>
+
+#define BAUD_RATE 114200
+
+#define PN532_IRQ   (2)
+#define PN532_RESET (3)
+
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
+bool nfc_enabled = true;
 
 
 
-// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 
 
-const byte ledPin = PB1;  	//LED D9
-const byte interruptPin = 2;  	//Button D2
-
+const uint8_t ledPin = PB1;  	//LED D9
+const uint8_t interruptPin = 2;  	//Button D2
 
 //volatiles
 //how long the led should light, 1 = 500 ms
 volatile int ledControl = 0;
+volatile const uint8_t uid_length = 7;
 
 
 
 
-//Timer reset and timer compare goal
+//Timer1 reset and timer compA goal
 const uint16_t tl_load = 0;
 const uint16_t tl_comp = 31250;
 
@@ -46,67 +56,58 @@ const uint16_t tl_comp = 31250;
 
 void setup() {
 
-
   //init watchdog timer
   WDT_Prescaler_Change();
   //__watchdog_reset(); must be called periodically
 
 
+  lcd.begin(16, 2);
+  Serial.begin(BAUD_RATE);
+
+  //set ledPin to output
+  DDRB |= (1 << ledPin);
+
+  nfc.begin();
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (!versiondata) {
+    Serial.println("Didn't find PN53x board");
+    while (1); // halt
+  }
+  nfc.setPassiveActivationRetries(0x00);
+  nfc.SAMConfig();
+
+
+  //Button interrupt routine
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), buttonPressed, RISING);
 
 
 
 
+  timer1_setup();
 
-
-
-
-    lcd.begin(16, 2);
-    Serial.begin(9600);
-
-    //Button interrupt routine
-    pinMode(interruptPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(interruptPin), buttonPressed, RISING);
-
-
-    //ledPin output
-    DDRB |= (1 << ledPin);
-
-
-    //reset timer1 control reg A
-    TCCR1A = 0;
-
-    //prescaler 256
-    TCCR1B |= (1 << CS12);
-    TCCR1B &= ~(1 << CS11);
-    TCCR1B &= ~(1 << CS10);
-
-    //reset timer 1 and set compare value
-    TCNT1 = tl_load;
-    OCR1A = tl_comp;
-
-    //enable timer1 compare interrupt
-    TIMSK1 = (1 << OCIE1A);
-
-    //enable global interrupt
-    sei();
 }
 
 
 void loop() {
+ __watchdog_reset();
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
 
-    bool uid_success = false;
 
 
-/*UID READ
-*If UID read && accepted, set ledControl to 5(1=500ms)
-*save/send used UID and timestamp to webservice
+  if (nfc_enabled) {
+    if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uid_length)) {
 
-*If UID read && unaccepted, set ledControl negative(led goes dark for x*500ms)
-*save/send used UID and timestamp to webservice
-*/
-    if (uid_success) {
-        ledControl = 5;
+      //check if UID is on EEPROM
+      if(checkUID(uid)){
+        //UID found - do things
+        ledControl = 10;
+      } else {
+        //UID not found - do other things
+        ledControl = -10;
+      }
     }
+  }
     delay(100);
 }
 
@@ -123,8 +124,6 @@ ISR(TIMER1_COMPA_vect) {
     TCNT1 = tl_load;
 
     if (ledControl > 0) {
-        //if uid accepted, burn led
-
         //Check if led is off
         if (!(PINB & (1 << 1))) {
             PORTB ^= (1 << ledPin);
@@ -133,8 +132,6 @@ ISR(TIMER1_COMPA_vect) {
     }
     else if (ledControl < 0)
     {
-        //if uid unaccepted, shut led
-
         //check if led is on
         if (PINB & (1 << 1)) {
             PORTB ^= (1 << ledPin);
@@ -154,65 +151,34 @@ ISR(TIMER1_COMPA_vect) {
 Button to interrupt UID read
 Used to write new accepted UIDs to memory
 
-*EEPROM - UID is 7 bytes, EEPROM is 1024 bytes
-*Use first bytes of UID for address control
-!!***Atmel ATmega328P datasheet page 20-23 contains readymade functions to write/read EEPROM on register level***!!
-
-Use keypad to lock the adding of new UIDs if there's time
+*EEPROM - UID is uid_length(4 or 7) bytes, EEPROM is 1024 bytes
+*Use first byte of EEPROM for UID address control
 */
 void buttonPressed() {
-  //Used to compare bytes of new UID to those already saved
-  int numCheck = 0;
+
+  uint8_t newUid[] = {0,0,0,0,0,0,0};
   
-  ledControl = 10;
-  byte newUid[7] = {0,0,0,0,0,0,0};
-  byte numberOfUID = 0; //How many UIDS saved, Saved in EEPROM address 0
-  //Read UID
-  if(readSuccess){
-
-
-
-    //check if UID is already in EEPROM
-    if(numberOfUID > 0){
-      //go through every saved UID
-      for(int k = 0; k < numberOfUid; k++){
-        
-        //compare every byte of saved UID at address j+k*7 to those of newUID
-        for(int j = 0; j < 7; j++){
-          if(newUid[j] == EEPROM_read((j+k*7)){
-            numCheck++;
-          }
-        }
-        
-        
-        if(numCheck == 7){
-        //UID same as the last one saved
-        }
-
-        //reset numCheck if it didn't hit 7
-        numCheck = 0;
-      }
-    }
-
-
-
-    if(UID == FOUND){
-      //Don't need to save same UID twice, ignore it
-    
-    } else {
-      //NEW UID FOUND - write it to EEPROM
-
-      for(int i; i < 7; i++){
+  //read UID
+  if (nfc_enabled) {
+    if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &newUid[0], &uid_length)) {
+      //check if UID is already in EEPROM
+      if(checkUID(newUid)){
+      //UID already found in EEPROM
+      } else {
+        //NEW UID FOUND - write it to EEPROM
+        for(int i; i < uid_length; i++){
         //Write the first byte of new UID to lastaddress+1
-        EEPROM_write(((numberOfUID*7+1), newUid[i]))
+        EEPROM_write(((numberOfUID*uid_length+1), newUid[i]))
+        }
+        //increment saved UIDs
+        int num = EEPROM_read(0);
+        EEPROM_write(0, num+1);
       }
-      //increment saved UIDs
-      numberOfUID++;
     }
-
   }
 
 }
+
 
 
 
@@ -254,3 +220,54 @@ EECR |= (1<<EERE);
 /* Return data from Data Register */
 return EEDR;
 }
+
+
+
+
+//returns true if UID found on EEPROM
+bool checkUID(uint8_t uid[]){
+  //Used to compare bytes of new UID to those already saved
+  int numCheck = 0;
+   
+   if(EEPROM_read(0) > 0){
+      //go through every saved UID
+      for(int k = 0; k < numberOfUid; k++){
+        
+        //compare every byte of saved UID at address j+k*uid_length to those of newUID
+        for(int j = 0; j < uid_length; j++){
+          if(newUid[j] == EEPROM_read((j+k*uid_length)){
+            numCheck++;
+          }
+        }
+        if(numCheck == uid_length){
+        return true;
+        }
+
+        //reset numCheck if it didn't hit uid_length
+        numCheck = 0;
+      }
+    }
+return false;
+}
+
+
+void timer1_setup(){
+  //reset timer1 control reg A
+  TCCR1A = 0;
+
+  //prescaler 256
+  TCCR1B |= (1 << CS12);
+  TCCR1B &= ~(1 << CS11);
+  TCCR1B &= ~(1 << CS10);
+
+  //reset timer 1 and set compare value
+  TCNT1 = tl_load;
+  OCR1A = tl_comp;
+
+  //enable timer1 compare interrupt
+  TIMSK1 = (1 << OCIE1A);
+
+  //enable global interrupt
+  sei();
+}
+
