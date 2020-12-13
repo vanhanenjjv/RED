26759d6ea72aed7f83102bb38fe1dfcaf1eea200
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2020-12-13 20:41:12 +0200

    Moved Serial I/O to separate functions

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -11,6 +11,55 @@ Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
 bool nfc_enabled = true;
 
+void write_uid(void) {
+
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 }; // Buffer to store the returned UID
+  uint8_t uid_length;				               // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+  if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uid_length)) {
+
+    StaticJsonDocument<256> doc;
+    String uid_str;
+
+    for (int i = 0; i < uid_length; ++i) 
+        uid_str.concat(uid[i]);
+
+    doc["type"] = "CARD";
+    doc["uid"] = uid_str;
+
+    serializeJson(doc, Serial);
+    Serial.println();
+  }
+}
+
+void set_nfc_state(void) {
+  StaticJsonDocument<256> doc;
+  deserializeJson(doc, Serial);
+
+  int type = doc["type"].as<int>();
+
+  switch (type) {
+
+    case 0:
+      if (nfc_enabled) { // Ignore command when mode already in use
+        nfc_enabled = false;
+        Serial.println("NFC disabled");
+      }
+      break;
+
+    case 1:
+      if (!nfc_enabled) { // Ignore command when mode already in use
+        nfc_enabled = true;
+        Serial.println("NFC enabled");
+      }
+      break;
+    
+    default:
+      Serial.println("Mode not implemented");
+      break;
+  }
+}
+
 void setup(void) {
   Serial.begin(BAUD_RATE);
 
@@ -34,40 +83,10 @@ void setup(void) {
 
 void loop(void) {
   if (nfc_enabled) {
-    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 }; // Buffer to store the returned UID
-    uint8_t uid_length;				               // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-
-    if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uid_length)) {
-      StaticJsonDocument<256> doc;
-
-      String uid_str;
-
-      for (int i = 0; i < uid_length; ++i) 
-        uid_str.concat(uid[i]);
-
-      doc["type"] = "CARD";
-      doc["uid"]  = uid_str;
-
-      serializeJson(doc, Serial);
-      Serial.println();
-    }
+    write_uid();
   }
 
   while (Serial.available()) {
-    StaticJsonDocument<256> doc;
-    deserializeJson(doc, Serial);
-
-    int type = doc["type"].as<int>();
-
-    switch (type) {
-       case 0: {
-        nfc_enabled = false;
-        break;
-      }
-      case 1: {
-        nfc_enabled = true;
-        break;
-      }
-    }
+    set_nfc_state();
   }
 }
